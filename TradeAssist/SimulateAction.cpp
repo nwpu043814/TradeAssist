c95bdc2d609cf81b3f65e4324a1aebb2b4ec7f2e
denverhan <han_lian@foxmail.com>
2016-05-22 20:42:17 +0800

    fix auto submit failed

---
+++ TradeAssist/SimulateAction.cpp
@@ -0,0 +1,76 @@
+#include "StdAfx.h"
+#include "SimulateAction.h"
+#include "Constant.h"
+
+SimulateAction::SimulateAction(void)
+{
+}
+
+SimulateAction::~SimulateAction(void)
+{
+}
+
+// 模拟鼠标双击
+int SimulateAction::MouseDoubleClick(void)
+{
+	MouseClick();
+	Sleep(MOUSE_DOUBLE_CLICK_DELAY);
+	MouseClick();
+	return 0;
+}
+
+// 鼠标单击
+int SimulateAction::MouseClick(void)
+{
+	POINT lpPoint;
+	GetCursorPos(&lpPoint);
+	SetCursorPos(lpPoint.x, lpPoint.y);
+
+	mouse_event(MOUSEEVENTF_LEFTDOWN,0,0, NULL,NULL);
+	Sleep(MOUSE_CLICK_DELAY);
+	mouse_event(MOUSEEVENTF_LEFTUP,0,0, NULL,NULL);
+	
+	return 0;
+}
+
+// 通过模拟Ctrl+C来复制
+int SimulateAction::KeyboardCopy(void)
+{
+	keybd_event(VK_CONTROL ,0,0,0);
+	keybd_event(VK_C ,0,0,0);
+	Sleep(KEYBD_DELAY);
+	keybd_event(VK_C,0,KEYEVENTF_KEYUP,0); 
+	keybd_event(VK_CONTROL,0,KEYEVENTF_KEYUP,0); 
+	return 0;
+}
+
+// 通过键盘Ctrl+V模拟粘贴
+int SimulateAction::KeyboardPaste(void)
+{
+	keybd_event(VK_CONTROL ,0,0,0);
+	keybd_event(VK_V ,0,0,0);
+	Sleep(KEYBD_DELAY);
+	keybd_event(VK_V,0,KEYEVENTF_KEYUP,0); 
+	keybd_event(VK_CONTROL,0,KEYEVENTF_KEYUP,0); 
+	return 0;
+}
+
+//移动鼠标
+int SimulateAction::MoveCursor(int dx, int dy, bool isAbslute)
+{
+	POINT lpPoint;
+	GetCursorPos(&lpPoint);
+
+	if (isAbslute)
+	{
+		SetCursorPos(dx, dy);
+	}
+	else
+	{
+		SetCursorPos(lpPoint.x + dx, lpPoint.y + dy);
+	}
+
+
+
+	return 0;
+}
---
+++ TradeAssist/SimulateAction.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class SimulateAction
+{
+public:
+	SimulateAction(void);
+	virtual ~SimulateAction(void);
+	// 模拟鼠标双击
+	int MouseDoubleClick(void);
+	// 鼠标单击
+	int MouseClick(void);
+	// 通过模拟Ctrl+C来复制
+	int KeyboardCopy(void);
+	// 通过键盘Ctrl+V模拟粘贴
+	int KeyboardPaste(void);
+	int MoveCursor(int dx, int dy, bool b = false);
+};
